ec7a883da895926d4e811bef96b77f72ff69fa6e
Harry Bagdi <harrybagdi@gmail.com>
2017-12-28 16:15:24 -0800

    feat: add random data personal data generation
    Following details can now be randomly generated
    
    * Name
    * E-mail
    * Cell
    * SSN
    * Credit-card number (16-digits)

--- main.go
+++ main.go
@@ -16,9 +16,47 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "rand"
 	app.Usage = "generate semantic psuedo-random data"
-	app.Action = func(c *cli.Context) error {
-		fmt.Println(gofakeit.Name())
-		return nil
+	app.Commands = []cli.Command{
+		{
+			Name:  "name",
+			Usage: "generate a random (first and last) name of person",
+			Action: func(c *cli.Context) error {
+				fmt.Println(gofakeit.Name())
+				return nil
+			},
+		},
+		{
+			Name:  "email",
+			Usage: "generate a random e-mail address",
+			Action: func(c *cli.Context) error {
+				fmt.Println(gofakeit.Email())
+				return nil
+			},
+		},
+		{
+			Name:  "cell",
+			Usage: "generate a random cellular number",
+			Action: func(c *cli.Context) error {
+				fmt.Println(gofakeit.Phone())
+				return nil
+			},
+		},
+		{
+			Name:  "cc",
+			Usage: "generate a random credit card number",
+			Action: func(c *cli.Context) error {
+				fmt.Println(gofakeit.CreditCardNumber())
+				return nil
+			},
+		},
+		{
+			Name:  "ssn",
+			Usage: "generate a random US Social Security Number (SSN)",
+			Action: func(c *cli.Context) error {
+				fmt.Println(gofakeit.SSN())
+				return nil
+			},
+		},
 	}
 	app.Run(os.Args)
 }
